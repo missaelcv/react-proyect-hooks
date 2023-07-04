6e6cab36d62e8e2ce92fc12d870afcafabb23b28
missaelcv <missaelcv0220@gmail.com>
2023-07-04 14:27:37 -0400

    User SetNumero for Hooks

--- src/Components/Contador.js
+++ src/Components/Contador.js
@@ -5,11 +5,14 @@ const Contador = () => {
 
     const [numero, setNumero] = useState(0);
 
-    
+    const aumentar = () => {
+        console.log('Se Pulso El CLic')
+        setNumero(numero + 1);
+    }
     return ( 
         <Fragment>
             <h3>First Componente {numero}</h3>
-        <button onClick={}>Acceptar</button>
+        <button onClick={aumentar}>Acceptar</button>
         </Fragment>
      );
 }
